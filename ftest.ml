e83aa43e0c14384ff4f91d826a98bd3313828859
Vincent Erb <vincent.erb@hotmail.fr>
2018-11-19 17:20:06 +0100

    added general purpose test file

--- ftest.ml
+++ ftest.ml
@@ -3,30 +3,21 @@ open Ff
 
 let () =
 
-  if Array.length Sys.argv <> 5 then
+  if Array.length Sys.argv <> 4 then
     begin
-      Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
+      Printf.printf "\nUsage: infile origin dest \n\n%!" ;
       exit 0
     end ;
 
-  let infile = Sys.argv.(1)
-  and outfile = Sys.argv.(4)
-  
-  (* These command-line arguments are not used for the moment. *)
-  and _source = Sys.argv.(2)
-  and _sink = Sys.argv.(3)
+  let infile = Sys.argv.(1) and
+  origin = Sys.argv.(2) and
+  dest = Sys.argv.(3)
   in
-
   (* Open file *)
   let graph = Gfile.from_file infile in
 	let fg=Ff.init (Graph.map graph int_of_string)
 	in
-	let path_i=Ff.find_path fg "4" "1"
+	let result = run fg origin dest
 	in
-	match path_i with
-		|Some(c) -> 
-			(* Rewrite the graph that has been read. *)
-			let () = Ff.export_path outfile fg "4" "1" c in
-			()
-		|None -> let ()=Printf.printf "Failed %!" in ()
+	Printf.printf "%s \n %!" (string_of_int result)
  

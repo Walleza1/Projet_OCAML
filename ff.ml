86a42604ad7154b0987d407ea7036af75ee9b3e0
Vincent Erb <vincent.erb@hotmail.fr>
2018-11-26 14:46:50 +0100

    Cleaned up non ecart functions, resolves #1

--- ff.ml
+++ ff.ml
@@ -1,25 +1,19 @@
 open Graph
 open Gfile
 
-type ford_graph = (int*int) graph
+(** Actu, Cap, Cost*)
+type ford_graph = (int*int*int) graph
 
 type ford_ecart_graph = (int*bool) graph
 
 type ford_path = id list
 
 (** Function init **)
-let init graph = map graph (fun x -> (0,x))
+let init graph = map graph (fun x y -> (0,x,y))
 ;;
-
-
-(** Boolean to test if this arc is available *)
-let is_arc_available (id,(actu,cap)) =
-	actu < cap
-;;
-
 
 (** Returns actual flow value of an arc *)
-let value_of_arc (id,(actu,cap)) = actu;;
+let value_of_arc (id,(actu,cap,_)) = actu;;
 
 let to_fold ecart_g id l=
 	let graph=
@@ -29,7 +23,7 @@ let to_fold ecart_g id l=
 			|Graph_error(e)->ecart_g
 	in
 	let rec loop acu li=match li with
-		|(nid,(cap,max_cap))::rest-> 
+		|(nid,(cap,max_cap,_))::rest-> 
 			let gr=try
 				add_node acu nid
 			with
@@ -145,7 +139,7 @@ let update_ford_graph_ecart (graph:ford_graph) path=
 								in
 									begin
 										match arc_actu with
-											|Some(actu,cap)->let nvalue=actu-value in loop (b::rest) (add_arc acc b a (nvalue,cap))
+											|Some(actu,cap,cost)->let nvalue=actu-value in loop (b::rest) (add_arc acc b a (nvalue,cap,cost))
 											|None -> Printf.printf "arc %s - %s " a b ;failwith "Error, arc reverse is not valid"
 									end
 							else
@@ -153,7 +147,7 @@ let update_ford_graph_ecart (graph:ford_graph) path=
 								in
 									begin
 										match arc_actu with
-											|Some(actu,cap)->let nvalue=actu+value in loop (b::rest) (add_arc acc a b (nvalue,cap))
+											|Some(actu,cap,cost)->let nvalue=actu+value in loop (b::rest) (add_arc acc a b (nvalue,cap,cost))
 											|None-> failwith "Error arc is not valid"
 									end
 						|None->failwith "Error arc not found in ford_ecart_graph"
@@ -163,75 +157,6 @@ let update_ford_graph_ecart (graph:ford_graph) path=
 		loop path graph
 ;;
 
-(**Take a ford_graph and finda path *)
-let find_path (graph:ford_graph) origin destination=
-	let rec loop start acc visited =
-		if start = destination then
-			Some (List.rev acc)
-		else 
-			let arcs=List.filter is_arc_available (out_arcs graph start)
-			in
-			let rec try_path list_arc=match list_arc with
-				| [] -> None
-				|(id,(actu,cap))::rest-> 
-					if List.mem id visited then
-						None
-					else
-						let result=loop id (id::acc) (id::visited)
-						in 
-						begin
-							match result with
-								| None -> try_path rest
-								| _ -> result
-						end
-			in
-			try_path arcs
-	in
-		loop origin [origin] []	
-;;
-
-
-(**Get the maximum flow value available (total capacity - actual) in a path *)
-let get_path_max_value (graph:ford_graph) (path:ford_path)=
-	let rec loop pathi= match pathi with
-		| a::b::rest -> 
-			let arc=find_arc graph a b
-			in
-				begin
-				match arc with
-					|Some(actu,cap)-> (cap-actu)::(loop (b::rest))
-					|None -> failwith "Error, path is not valid in graph"
-				end
-		| _ -> []
-	in 
-	let path_values=loop path
-	in
-		try 
-			List.fold_left (fun x y -> Pervasives.min x y) (List.hd path_values) path_values
-		with 
-			| hd->0
-;;
-
-(**Get the maximum flow value available (total capacity - actual) in a path *)
-let update_ford_graph (graph:ford_graph) (path:ford_path)=
-	(* value >=0 *)
-	let value=get_path_max_value graph path
-	in
-	let rec loop pathi acc = match pathi with
-		|a::b::rest -> 
-			let arc_actu=find_arc graph a b
-			in
-			begin
-			match arc_actu with
-				|Some(actu,cap)->
-					let nvalue=actu+value 
-					in loop (b::rest) (add_arc acc a b (nvalue,cap))
-				|None -> failwith "Error, path is not valid"
-			end
-		| _ -> acc
-	in
-	loop path graph
-;;
 
 (**Sums the flow value of all outgoing arcs of node id *)
 let sum_outarcs_value graph id = 
@@ -240,23 +165,6 @@ let sum_outarcs_value graph id =
 		List.fold_left (+) 0 (List.map value_of_arc arcs)
 ;;
 
-(**Takes a ford graph, an origin and destination node id, and runs ford-fulkerson on it, returning the max flow value *)
-let run (graph:ford_graph) origin dest =
-	let rec loop gr = 
-	(* Tu avais utilisé graph du coup il partait toujours du graph initial donc toujours un chemin donc stack overflow *)
-	let path=find_path gr origin dest in
-	match (path) with
-		|Some(chemin) -> 
-			begin
-			(* Ajout d'un match case quand le chemin n'est composé que d'un élément ou est vide. Si on demande origin = dest il renvoit le chemine [origin] *)
-				match chemin with
-					|a::b::rest ->loop (update_ford_graph gr chemin)
-					| _ -> sum_outarcs_value gr origin
-			end
-		|None -> sum_outarcs_value gr origin
-	in
-	loop graph
-;;
 
 (**Takes a ford graph, an origin and destination node id, and runs ford-fulkerson on it, returning the max flow value *)
 let run_ecart (graph:ford_graph) origin dest =
@@ -265,7 +173,7 @@ let run_ecart (graph:ford_graph) origin dest =
 		in
 		let path=find_path_ecart gr_ecart origin dest in
 	match (path) with
-		|Some(chemin) -> 
+	|Some(chemin) -> 
 			begin
 			(* Ajout d'un match case quand le chemin n'est composé que d'un élément ou est vide. Si on demande origin = dest il renvoit le chemine [origin] *)
 				match chemin with
@@ -281,7 +189,7 @@ let run_ecart (graph:ford_graph) origin dest =
 
 
 (* Create a string graph *)
-let toString (graph:ford_graph) = map graph (fun (a,b) -> (string_of_int a)^"/"^(string_of_int b))	
+let toString (graph:ford_graph) = map graph (fun (a,b,c) -> (string_of_int a)^"/"^(string_of_int b)^"/cost : "^(string_of_int c))	
 
 let export path (graph:ford_graph) origin destination=
  let (o,d)=((node_exists graph origin),(node_exists graph destination))
@@ -309,23 +217,22 @@ let export_path file_path (graph:ford_graph) origin destination (path:ford_path)
 		|(_,_) -> raise (Graph_error("Nodes aren't present in graph"))
 
 
-(**Takes a ford graph, an origin and destination node id, and runs ford-fulkerson on it, returning the max flow value. At the end render dotfile at path **)
-let run_and_print file_path (graph:ford_graph) origin dest =
-	let rec loop gr = 
-	(* Tu avais utilisé graph du coup il partait toujours du graph initial donc toujours un chemin donc stack overflow *)
-	let path=find_path gr origin dest in
-	match (path) with
-		|Some(chemin) -> 
-			begin
-			(* Ajout d'un match case quand le chemin n'est composé que d'un élément ou est vide. Si on demande origin = dest il renvoit le chemine [origin] *)
-				match chemin with
-					|a::b::rest ->loop (update_ford_graph gr chemin)
-					| _ -> export file_path gr origin dest ;sum_outarcs_value gr origin
-			end
-		|None -> sum_outarcs_value gr origin
-	in
-	loop graph
-;;
 
-
-
+	
+		let run_and_print file_path (graph:ford_graph) origin dest =
+			let rec loop gr = 
+				let gr_ecart=create_ecart_graph gr
+				in
+				let path=find_path_ecart gr_ecart origin dest in
+			match (path) with
+			|Some(chemin) -> 
+					begin
+					(* Ajout d'un match case quand le chemin n'est composé que d'un élément ou est vide. Si on demande origin = dest il renvoit le chemine [origin] *)
+						match chemin with
+							|a::b::rest ->loop (update_ford_graph_ecart gr chemin)
+							| _ -> export file_path gr origin dest ;sum_outarcs_value gr origin
+					end
+				|None -> sum_outarcs_value gr origin
+			in
+			loop graph
+		;;
